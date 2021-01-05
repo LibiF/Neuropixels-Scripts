d568a7146635bb56e1df69954c8ee46a759a9090
LibiF <38905822+LibiF@users.noreply.github.com>
2021-01-05 22:34:41 +0200

    Delete write_audio_func.m

--- write_audio_func.m
+++
@@ -1,6 +0,0 @@
-up = load('D:\Libi\Category stimuli\Diff_slope\Rise_to_pure_tone160.mat') ;
-up = up.stim_up ;
-% down = load('C:\Users\owner\Dropbox\Category stimuli\Teaching_stims_1sec\Down_base_12.mat') ;
-% down = down.stim_down ;
-audiowrite('puretone16.wav',up,500000)
-% audiowrite('down12_6.wav',down,500000)
